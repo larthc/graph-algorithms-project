dec475e7391f185000eef514ddec31dcfa4c6337
luisarth <158247497+larthc@users.noreply.github.com>
2026-03-11 15:38:05 -0300

    Update pokemon_graph.py

--- api-project/pokemon_graph.py
+++ api-project/pokemon_graph.py
@@ -1,1 +1,19 @@
+import requests
 
+def get_pokemon(name):
+
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    response = requests.get(url)
+
+    data = response.json()
+
+    return data
+
+
+pokemon = get_pokemon("pikachu")
+
+print("Nome:", pokemon["name"])
+
+print("Tipos:")
+for t in pokemon["types"]:
+    print("-", t["type"]["name"])
